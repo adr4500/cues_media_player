96349b26db8dd70267df770c0f1ca7e5054735a4
Adrien Bigotte <adr4500@gmail.com>
2023-04-15 19:13:34 +0200

    Added the video window

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -9,6 +9,7 @@
 #include <JuceHeader.h>
 #include "MainComponent.h"
 #include "ExternalInfo.h"
+#include <cassert>
 
 //==============================================================================
 class CMPApplication : public juce::JUCEApplication
@@ -41,7 +42,7 @@ public:
                 juce::ModalCallbackFunction::create ([] (int) { juce::MessageManager::getInstance()->stopDispatchLoop(); }));
             return;
         }
-        if (not externalInfo.setVideoPath ("Video.mp4"))
+        if (not externalInfo.setVideoPath ("Video.avi"))
         {
             juce::AlertWindow::showMessageBoxAsync (
                 juce::AlertWindow::AlertIconType::WarningIcon,
@@ -63,15 +64,17 @@ public:
                 juce::ModalCallbackFunction::create ([] (int) { juce::MessageManager::getInstance()->stopDispatchLoop(); }));
             return;
         }
-
-        mainWindow.reset (new MainWindow (getApplicationName ()));
+        videoWindow.reset (new VideoWindow (getApplicationName ()));
+        videoWindow.get()->loadVideo(externalInfo.getVideoFile());
+        controlPannelWindow.reset (new ControlPannelWindow (getApplicationName ()));
     }
 
     void shutdown () override
     {
         // Add your application's shutdown code here..
 
-        mainWindow = nullptr; // (deletes our window)
+        controlPannelWindow = nullptr; // (deletes our window)
+        videoWindow = nullptr;
     }
 
     //==============================================================================
@@ -92,13 +95,12 @@ public:
 
     //==============================================================================
     /*
-        This class implements the desktop window that contains an instance of
-        our MainComponent class.
+        This class implements the desktop window that contains the cues and the pause button.
     */
-    class MainWindow : public juce::DocumentWindow
+    class ControlPannelWindow : public juce::DocumentWindow
     {
     public:
-        MainWindow (juce::String name)
+        ControlPannelWindow (juce::String name)
             : DocumentWindow (
                   name,
                   juce::Desktop::getInstance ()
@@ -136,11 +138,85 @@ public:
         */
 
     private:
-        JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainWindow)
+        JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ControlPannelWindow)
+    };
+
+    /*
+        This class implements the fullscreen video window.
+    */
+    class VideoWindow : public juce::DocumentWindow
+    {
+    public:
+        VideoWindow (juce::String name)
+            : DocumentWindow (
+                  name,
+                  juce::Desktop::getInstance ()
+                      .getDefaultLookAndFeel ()
+                      .findColour (juce::ResizableWindow::backgroundColourId),
+                  DocumentWindow::allButtons)
+        {
+            setUsingNativeTitleBar (true);
+            setContentOwned (new juce::VideoComponent (true), true);
+            setFullScreen (false);
+            setVisible (true);
+        }
+
+        void closeButtonPressed () override
+        {
+            // This is called when the user tries to close this window. Here,
+            // we'll just ask the app to quit when this happens, but you can
+            // change this to do whatever you need.
+            JUCEApplication::getInstance ()->systemRequestedQuit ();
+        }
+
+        void loadVideo (juce::File videoFile)
+        {
+            juce::VideoComponent* videoComponent = dynamic_cast<juce::VideoComponent*> (getContentComponent ());
+            assert(videoComponent != nullptr);
+            if (videoComponent == nullptr)
+            {
+                juce::AlertWindow::showMessageBoxAsync (
+                    juce::AlertWindow::AlertIconType::WarningIcon,
+                    "Fatal Error",
+                    "The video component could not be found",
+                    "OK",
+                    nullptr,
+                    juce::ModalCallbackFunction::create ([] (int) { juce::MessageManager::getInstance ()->stopDispatchLoop (); }));
+                return;
+            }
+
+            auto result = videoComponent->load (videoFile);
+            if(result.failed())
+            {
+                juce::AlertWindow::showMessageBoxAsync (
+                    juce::AlertWindow::AlertIconType::WarningIcon,
+                    "Error",
+                    "The video could not be loaded : " + result.getErrorMessage(),
+                    "OK",
+                    nullptr,
+                    juce::ModalCallbackFunction::create ([] (int) { juce::MessageManager::getInstance()->stopDispatchLoop(); }));
+            }
+            else
+            {
+                videoComponent->play();
+            }
+        }
+
+        /* Note: Be careful if you override any DocumentWindow methods - the
+           base class uses a lot of them, so by overriding you might break its
+           functionality. It's best to do all your work in your content
+           component instead, but if you really have to override any
+           DocumentWindow methods, make sure your subclass also calls the
+           superclass's method.
+        */
+
+    private:
+        JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (VideoWindow)
     };
 
 private:
-    std::unique_ptr<MainWindow> mainWindow;
+    std::unique_ptr<ControlPannelWindow> controlPannelWindow;
+    std::unique_ptr<VideoWindow> videoWindow;
     CMP::ExternalInfo externalInfo;
 };
 
