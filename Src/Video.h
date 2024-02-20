ed3afdaba4dd2ed8369913029f731f3b4b22e223
Adrien Hervé <adr4500@gmail.com>
2024-02-20 19:52:34 +0100

    Migrate to VLC part 1

--- Src/Video.h
+++ Src/Video.h
@@ -1,25 +1,17 @@
 #pragma once
 
-#include <gst/gst.h>
+#include <sys/types.h>
+#include <vlc/vlc.h>
+#include "MediaPlayer.hpp"
 #include <JuceHeader.h>
 #include "VideoMessage.h"
 
 namespace CMP
 {
 
-struct DemuxOutput
-{
-    DemuxOutput (GstElement* _audioFlowInput, GstElement* _videoFlowInput)
-        : audioFlowInput (_audioFlowInput), videoFlowInput (_videoFlowInput)
-    {
-    }
-    GstElement* audioFlowInput{nullptr};
-    GstElement* videoFlowInput{nullptr};
-};
-
 //==============================================================================
 /*
-    This class represents a video. It manages all low level gstreamer calls.
+    This class represents a video. It manages all VLC related operations
 */
 class Video : public juce::MessageListener
 {
@@ -42,7 +34,7 @@ public:
     static void setMainThread (juce::MessageListener* _mainThread);
 
     //==============================================================================
-    guint64 getRunningTime () const;
+    long long int getRunningTime () const;
 
 private:
     class VideoThread : public juce::Thread
@@ -61,43 +53,12 @@ private:
 
         bool isPlaying () const;
 
-        guint64 getRunningTime () const;
+        long long int getRunningTime () const;
 
     private:
-        static gboolean
-        busCallback (GstBus* _bus, GstMessage* _msg, gpointer _data);
-
-        static void
-        onPadAdded (GstElement* _element, GstPad* _pad, gpointer _data);
-        static GstPadProbeReturn encoderCbHaveData (GstPad* pad,
-                                                    GstPadProbeInfo* info,
-                                                    gpointer user_data);
-
-        void clean ();
-
-        GstElement* pipeline{nullptr};
-        GstElement* source{nullptr};
-        GstElement* demux{nullptr};
-
-        GstElement* videoqueue{nullptr};
-        GstElement* videoparser{nullptr};
-        GstElement* videodecoder{nullptr};
-        GstElement* videoconv{nullptr};
-        GstElement* videosink{nullptr};
-        GstElement* audioqueue{nullptr};
-        GstElement* audioparser{nullptr};
-        GstElement* audiodecoder{nullptr};
-        GstElement* audioconv{nullptr};
-        GstElement* audioresample{nullptr};
-        GstElement* audiosink{nullptr};
-
-        GMainLoop* loop;
-
-        DemuxOutput* separateFlows{nullptr};
-
-        GstBus* bus{nullptr};
-        guint busWatchId{0};
-        GstMessage* msg{nullptr};
+        VLC::Instance* instance;
+        VLC::MediaPlayer* player;
+        VLC::Media* media;
 
         bool playing{false};
 
