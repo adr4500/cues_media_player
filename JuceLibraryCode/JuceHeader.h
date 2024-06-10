b552e1c937d27d2ccdb0380d3b93533915f709fb
Adrien Hervé <adr4500@gmail.com>
2024-06-10 14:43:29 +0200

    Release v1.2.2

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -40,7 +40,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "CMP";
     const char* const  companyName    = "SiMDE";
-    const char* const  versionString  = "1.2.1";
-    const int          versionNumber  = 0x10201;
+    const char* const  versionString  = "1.2.2";
+    const int          versionNumber  = 0x10202;
 }
 #endif
